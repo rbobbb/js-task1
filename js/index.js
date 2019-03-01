e089d1f37df48f14eb4daff6e861f352463109e9
Roman <roman.bobyk@gmail.com>
2019-03-01 14:33:02 +0000

    prvk

--- js/index.js
+++ js/index.js
@@ -5,24 +5,21 @@ const c = parseFloat(prompt('Enter C'));
 const result = solveQuard(a, b, c);
 alert(result);
 
-
-function solveQuard(a, b, c){
-  const d  = calcDiscr(a, b, c);
-  if (d === 0){
-   x = -b / (2 * a);
-        return 'Result: x1 = ' + x; 
-    }  if (d > 0){
-    x1 = (-b + Math.sqrt(d)) / (2 * a);
-        x2 = (-b - Math.sqrt(d)) / (2 * a);
-        return 'Results: x1 = ' + x1 + ' , x2 = ' + x2;
+function solveQuard(a, b, c) {
+    const d = calcDiscr(a, b, c);
+    if (d === 0) {
+       const x = -b / (2 * a);
+        return 'Result: x1 = ' + x;
     }
-    else if (d < 0)
+    if (d > 0) {
+       const x1 = (-b + Math.sqrt(d)) / (2 * a);
+       const x2 = (-b - Math.sqrt(d)) / (2 * a);
+        return 'Results: x1 = ' + x1 + ' , x2 = ' + x2;
+    } else if (d < 0)
         return 'Discriminant is less than zero, try again';
 
 }
 
-
 function calcDiscr(a, b, c) {
     return b * b - 4 * a * c;
 }
-
